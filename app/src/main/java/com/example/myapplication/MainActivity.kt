e22f5af2b7461d78fcdb4dcf2b8d24f9883a07ee
spartan0226 <161425545+spartan0226@users.noreply.github.com>
2024-03-22 15:04:49 +0900

    first basic class assignment

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private var job: Job? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setupButton()
+        setRandomValueBetweenOneToHundred()
+        setJobAndLaunch()
+    }
+
+    private fun setupButton() {
+        val button = findViewById<Button>(R.id.clickButton)
+
+        button.setOnClickListener {
+            job?.cancel()
+            checkAnswerAndShowToast()
+        }
+    }
+
+    private fun setRandomValueBetweenOneToHundred() {
+        val randomTextView = findViewById<TextView>(R.id.textViewRandom)
+        val randomValue = (1..100).random()
+
+        randomTextView.setText(randomValue.toString())
+    }
+
+    private fun setJobAndLaunch() {
+        val textView = findViewById<TextView>(R.i`d.spartaTextView)
+
+        job = lifecycleScope.launch {
+            for (i in 1..100) {
+                if (isActive) {`
+                    textView.text = i.toString()
+                    delay(500)
+                }
+            }
+        }
+    }
+
+    private fun checkAnswerAndShowToast() {
+        val textView = findViewById<TextView>(R.id.spartaTextView)
+        val randomTextView = findViewById<TextView>(R.id.textViewRandom)
+
+        if(textView.text.toString() == randomTextView.text.toString()) {
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+        }
+        if(textView.text.toString() != randomTextView.text.toString()){
+            Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
